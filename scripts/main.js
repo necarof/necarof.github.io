051d9a2581f653c53c42c8df105e793c57779347
Nikita_Mastepanov <nikitampost@gmail.com>
2025-04-05 13:40:10 +0500

    itext hide

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,12 @@ Fancybox.bind('[data-fancybox="gallery"]', {
     //
 });
 
-Fancybox.bind('[data-fancybox="gallery-a"]', {
-    //
-});
+// Fancybox.bind('[data-fancybox="gallery-a"]', {
+//     //
+// });
 
 Fancybox.bind('[data-fancybox="gallery-b"]', {
     //
 });
+
+document.getElementById('hide_item').remove();
